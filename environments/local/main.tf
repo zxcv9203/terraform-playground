d048713896175984c5078ac4e6e630c3ba15067b
yongckim <zxcv9203@naver.com>
2024-06-19 22:43:24 +0900

    :sparkles: 로컬 스택을 활용한 로컬 환경 테라폼 정의

--- environments/local/main.tf
+++ environments/local/main.tf
@@ -2,7 +2,7 @@ module "s3" {
   source = "../../modules/s3"
 
   region = var.region
-  access_key = var.access_key
-  secret_access_key = var.secret_access_key
-  s3_bucket_name = var.s3_bukcket_name
+  s3_bucket_name = var.s3_bucket_name
+
+  use_localstack = true
 }
--- environments/local/variables.tf
+++ environments/local/variables.tf
@@ -4,18 +4,6 @@ variable "region" {
   default = "ap-northeast-2"
 }
 
-variable "access_key" {
-  description = "AWS Access Key"
-  type = string
-  default = "test"
-}
-
-variable "secret_access_key" {
-  description = "AWS Secret Access Key"
-  type = string
-  default = "test"
-}
-
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket to create"
   type = string
