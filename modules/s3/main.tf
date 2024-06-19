c7694403afa69682a0d045bc65d5a127e3c8463c
yongckim <zxcv9203@naver.com>
2024-06-19 22:41:17 +0900

    :sparkles: 테라폼 S3 모듈 추가

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,23 @@
 provider "aws" {
+  alias = "s3"
   region = var.region
-  access_key = var.access_key
-  secret_access_key = var.secret_access_key
+
+  shared_credentials_files = [var.shared_credentials_file]
+  profile = var.use_localstack ? "test" : "default"
+
+  endpoints {
+    s3 = var.use_localstack ? "http://127.0.0.1:4566" : null
+  }
+
+  skip_credentials_validation = var.use_localstack ? true : false
+  skip_metadata_api_check = var.use_localstack ? true : false
+  skip_requesting_account_id = var.use_localstack ? true : false
+
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "this" {
   provider = aws.s3
   bucket = var.s3_bucket_name
+
+  force_destroy = var.use_localstack ? true : false
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -4,17 +4,18 @@ variable "region" {
   default     = "ap-northeast-2"
 }
 
-variable "access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "secret_access_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-}
-
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
+}
+
+variable "use_localstack" {
+  description = "로컬 스택을 사용하여 인프라를 구성할 것인지 여부를 묻는 변수입니다. (로컬 전용)"
+  type = bool
+  default = false
+}
+
+variable "shared_credentials_file" {
+  description = "AWS 연결을 위한 Credentials 파일의 경로를 설정합니다."
+  default = "../../aws-credentials"
 }
